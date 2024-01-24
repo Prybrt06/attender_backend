7d5127c896112801c42336f3672692c0804bfdd3
Priyabrat  Duarah <priyabrat@Priyabrats-MacBook-Air.local>
2024-01-24 21:37:26 +0530

    schedule implemented

--- src/middleware/schedule.ts
+++ src/middleware/schedule.ts
@@ -1,38 +1,100 @@
 import prisma from "../db";
 
 export const createSchedule = async (req, res, next) => {
-	try {
-		const schedule = await prisma.schedule.create({
-			data: {
-				day: req.body.day,
-				subjectName: req.body.subjectName,
-				subjectCode: req.body.subjectCode,
-				belongsToUserId: req.user.id,
-			},
-		});
+  const { day, startTime, endTime, subjectName, subjectCode } = req.body;
 
-		res.status(201).json({ schedule: schedule });
-	} catch (e) {
-		e.type = "schedule";
-		next(e);
-	}
+  if (startTime >= endTime) {
+    res.status(400).json({ message: "Invalid starttime and endtime" });
+    return;
+  }
+
+  try {
+    const schedule = await prisma.schedule.create({
+      data: {
+        day: day,
+        startTime: startTime,
+        endTime: endTime,
+        subjectName: subjectName,
+        subjectCode: subjectCode,
+        belongsToUserId: req.user.id,
+      },
+    });
+
+    res.status(201).json({ schedule: schedule });
+  } catch (e) {
+    e.type = "schedule";
+    next(e);
+  }
 };
 
 export const getSchedules = async (req, res, next) => {
-	try {
-		const subjects = await prisma.schedule.findMany({
-			where: { belongsToUserId: req.user.id },
-			select: {
-				id: true,
-				subjectName: true,
-				subjectCode: true,
-				belongsToUserId: true,
-			},
-		});
+  try {
+    const schedules = await prisma.schedule.findMany({
+      where: { belongsToUserId: req.user.id },
+      select: {
+        id: true,
+        day: true,
+        subjectName: true,
+        subjectCode: true,
+        belongsToUserId: true,
+      },
+    });
 
-		res.status(201).json({ subjects: subjects });
-	} catch (e) {
-		e.type = "schedule";
-		next(e);
-	}
+    const days = [
+      "MONDAY",
+      "TUESDAY",
+      "WEDNESDAY",
+      "THURSDAY",
+      "FRIDAY",
+      "SATURDAY",
+      "SUNDAY",
+    ];
+
+    schedules.sort((a, b) => {
+      return days.indexOf(a.day) - days.indexOf(b.day);
+    });
+
+    res.status(201).json({ schedules: schedules });
+  } catch (e) {
+    e.type = "schedule";
+    next(e);
+  }
+};
+
+export const getScheduleByDay = async (req, res, next) => {
+  const day = req?.params.day;
+  const days = [
+    "MONDAY",
+    "TUESDAY",
+    "WEDNESDAY",
+    "THURSDAY",
+    "FRIDAY",
+    "SATURDAY",
+    "SUNDAY",
+  ];
+
+  if (!days.includes(day)) {
+    res.status(401).json({ message: "Invalid parameter" });
+    return;
+  }
+
+  try {
+    const schedules = await prisma.schedule.findMany({
+      where: {
+        belongsToUserId: req.user.id,
+        day: day,
+      },
+      select: {
+        id: true,
+        day: true,
+        subjectName: true,
+        subjectCode: true,
+        belongsToUserId: true,
+      },
+    });
+
+    res.status(200).json({ schedules: schedules });
+  } catch (e) {
+    e.type = "schedule";
+  }
 };
